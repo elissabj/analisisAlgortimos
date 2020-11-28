f1fd5d0af63af74800006f857844fa34c9ab6716
elissabj <56558029+elissabj@users.noreply.github.com>
2020-11-27 19:39:49 -0600

    códigos python3
    Transformada Discreta de Fourier en python 3

---
+++ Python/DFT.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+def dft(x):
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+arr = input().split()
+print(dft(arr))
